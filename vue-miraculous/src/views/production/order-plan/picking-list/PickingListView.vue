7b1c1ee4adcdd12e5fb056bfadc495420c0e64e0
Chaisuriya Soisungnoen <chaisuriyasoisungnoen@MacBook-Pro-khxng-Chaisuriya.local>
2023-08-28 14:36:56 +0700

    edit form

--- vue-miraculous/src/views/production/order-plan/picking-list/PickingListView.vue
+++ vue-miraculous/src/views/production/order-plan/picking-list/PickingListView.vue
@@ -1,62 +1,50 @@
 <template>
   <div class="container-fluid">
     <h2>ชื่อฟอร์ม</h2>
-    <div class="row">
-      <div class="col-sm-3">
-        <form>
-          <div class="form-group>">
-            <img src="@/assets/ring.jpg" class="img-rounded img-responsive" height="200">
+    <form>
+      <div class="row">
+        <div class="col-lg-2">
+          <img src="@/assets/ring.jpg" class="img-responsive avatar" height="200">
+          <button type="button" class="btn btn-primary center">upload image</button>
+        </div>
+        <div class="col-lg-10">
+          <div class="col-sm-6">
+            <div class="form-group>">
+              <label>แม่พิมพ์ : </label>
+              <input type="text" class="form-control" id="num_wo">
+            </div>
+            <div class="form-group>">
+              <label>เลขที่ W.O. : </label>
+              <input type="text" class="form-control" id="num_wo">
+            </div>
+            <div class="form-group>">
+              <label>ลำดับ W.O. : </label>
+              <input type="text" class="form-control" id="num_wo">
+            </div>
           </div>
-          <div class="form-group>">
-            <button type="button" class="btn btn-primary center">upload image</button>
+          <div class="col-sm-6">
+            <div class="form-group>">
+              <label>ใบจ่าย/รับคืนงาน : </label>
+              <input type="date" class="form-control" id="num_wo">
+            </div>
+            <div class="form-group>">
+              <label>Require Date : </label>
+              <input type="date" class="form-control" id="num_wo">
+            </div>
+            <div class="form-group>">
+              <label>รหัสลูกค้า : </label>
+              <input type="text" class="form-control" id="num_wo">
+            </div>
           </div>
-          <div class="form-group>">
-            <label>แม่พิมพ์ : </label>
-            <input type="text" class="form-control" id="num_wo">
+          <div class="col-sm-12">
+            <div class="form-group>">
+              <label>Remark : </label>
+              <input type="text" class="form-control" id="num_wo">
+            </div>
           </div>
-        </form>
+        </div>
       </div>
-      <div class="col-sm-4">
-        <form>
-          <div class="form-group>">
-            <label>เลขที่ W.O. : </label>
-            <input type="text" class="form-control" id="num_wo">
-          </div>
-          <div class="form-group>">
-            <label>ลำดับ W.O. : </label>
-            <input type="text" class="form-control" id="num_wo">
-          </div>
-          <div class="form-group>">
-            <label>รหัสสินค้า : </label>
-            <input type="text" class="form-control" id="num_wo">
-          </div>
-        </form>
-      </div>
-      <div class="col-sm-4">
-        <form>
-          <div class="form-group>">
-            <label>ใบจ่าย/รับคืนงาน : </label>
-            <input type="date" class="form-control" id="num_wo">
-          </div>
-          <div class="form-group>">
-            <label>Require Date : </label>
-            <input type="date" class="form-control" id="num_wo">
-          </div>
-          <div class="form-group>">
-            <label>รหัสลูกค้า : </label>
-            <input type="text" class="form-control" id="num_wo">
-          </div>
-        </form>
-      </div>
-      <div class="col-sm-9">
-        <form>
-          <div class="form-group>">
-            <label>Remark : </label>
-            <input type="date" class="form-control" id="num_wo">
-          </div>
-        </form>
-      </div>
-    </div>
+    </form>
   </div>
 </template>
 
@@ -65,7 +53,7 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-  .center {
-    position: center;
-  }
+.center {
+  position: center;
+}
 </style>
