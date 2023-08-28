846474fea243458ae853565651472a9bb5a51a59
Chaisuriya Soisungnoen <chaisuriyasoisungnoen@MacBook-Pro-khxng-Chaisuriya.local>
2023-08-28 13:36:40 +0700

    form add data

--- vue-miraculous/src/views/production/order-plan/picking-list/PickingListView.vue
+++ vue-miraculous/src/views/production/order-plan/picking-list/PickingListView.vue
@@ -1,6 +1,62 @@
 <template>
-  <div class="container">
+  <div class="container-fluid">
     <h2>ชื่อฟอร์ม</h2>
+    <div class="row">
+      <div class="col-sm-3">
+        <form>
+          <div class="form-group>">
+            <img src="@/assets/ring.jpg" class="img-rounded img-responsive" height="200">
+          </div>
+          <div class="form-group>">
+            <button type="button" class="btn btn-primary center">upload image</button>
+          </div>
+          <div class="form-group>">
+            <label>แม่พิมพ์ : </label>
+            <input type="text" class="form-control" id="num_wo">
+          </div>
+        </form>
+      </div>
+      <div class="col-sm-4">
+        <form>
+          <div class="form-group>">
+            <label>เลขที่ W.O. : </label>
+            <input type="text" class="form-control" id="num_wo">
+          </div>
+          <div class="form-group>">
+            <label>ลำดับ W.O. : </label>
+            <input type="text" class="form-control" id="num_wo">
+          </div>
+          <div class="form-group>">
+            <label>รหัสสินค้า : </label>
+            <input type="text" class="form-control" id="num_wo">
+          </div>
+        </form>
+      </div>
+      <div class="col-sm-4">
+        <form>
+          <div class="form-group>">
+            <label>ใบจ่าย/รับคืนงาน : </label>
+            <input type="date" class="form-control" id="num_wo">
+          </div>
+          <div class="form-group>">
+            <label>Require Date : </label>
+            <input type="date" class="form-control" id="num_wo">
+          </div>
+          <div class="form-group>">
+            <label>รหัสลูกค้า : </label>
+            <input type="text" class="form-control" id="num_wo">
+          </div>
+        </form>
+      </div>
+      <div class="col-sm-9">
+        <form>
+          <div class="form-group>">
+            <label>Remark : </label>
+            <input type="date" class="form-control" id="num_wo">
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,4 +64,8 @@
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .center {
+    position: center;
+  }
+</style>
